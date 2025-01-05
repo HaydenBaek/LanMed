7e06a00ff8a99ea9af3dfe0c9954d53238d55170
Seonuk Baek <sbaek19@my.bcit.ca>
2025-01-05 01:50:52 -0800

    fixed viteconfig file

--- lanmed-frontend/vite.config.js
+++ lanmed-frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensures Vite builds to /dist (default behavior)
+    emptyOutDir: true,  // Clears old files in /dist before building new ones
+  }
 })
