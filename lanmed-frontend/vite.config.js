d78c6a9fe77545b203035160915172cb78b15879
Seonuk Baek <sbaek19@my.bcit.ca>
2025-01-05 06:33:48 -0800

    chagned firabse

--- lanmed-frontend/vite.config.js
+++ lanmed-frontend/vite.config.js
@@ -2,17 +2,15 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+  
   const env = loadEnv(mode, process.cwd());
+  
   return {
     plugins: [react()],
     define: {
+
       'import.meta.env': {
-        VITE_API_KEY: process.env.VITE_API_KEY,
-        VITE_AUTH_DOMAIN: process.env.VITE_AUTH_DOMAIN,
-        VITE_PROJECT_ID: process.env.VITE_PROJECT_ID,
-        VITE_STORAGE_BUCKET: process.env.VITE_STORAGE_BUCKET,
-        VITE_MESSAGING_SENDER_ID: process.env.VITE_MESSAGING_SENDER_ID,
-        VITE_APP_ID: process.env.VITE_APP_ID
+        ...env
       }
     }
   };
