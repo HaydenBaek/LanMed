02bd8a3f00e9e614872e2ed032970f1a354b89f3
Seonuk Baek <sbaek19@my.bcit.ca>
2025-01-05 05:23:10 -0800

    added clean script

--- lanmed-frontend/src/firebase.js
+++ lanmed-frontend/src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase config using environment variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -12,7 +11,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
